c382a535eecabb3394755f9854fcb84035469ede
Nam Nguyen <namnh11@gmail.com>
2024-11-10 18:19:18 -0800

    Create prefabs and basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float inputAxis;
+    private Vector2 velocity;
+    private new Rigidbody2D rigidbody;
+
+    public float moveSpeed = 8f;
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update() {
+        HorizontalMovement();
+    }
+
+    private void HorizontalMovement() {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x,inputAxis * moveSpeed,moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate() {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+        rigidbody.MovePosition(position);
+    }
+}

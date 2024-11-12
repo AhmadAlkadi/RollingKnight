ea765eb6e501ce0c70d0b6d9908597fd9672877d
Sean Butler <seanxbutler@gmail>
2024-11-12 11:56:14 -0800

    switch to using a single sprite sheet for all animations
    minor update in the look of the ball
    
    update of the animation tree to reflect the update of the ball animations for both idle and movement animations
    
    update the particle system values to make them look more like a trail and to have them not collide witht he player

--- Assets/Test Scripts/test_anim_and_particles.cs
+++ Assets/Test Scripts/test_anim_and_particles.cs
@@ -6,9 +6,14 @@ public class test_anim_and_particles : MonoBehaviour
     public float speed = 1.0f;
     public float particleOffsetX = 0.0f;
 
+    private float idleDir = -1.0f;
     private Vector2 movement;
+    private Vector2 moveDirection;
     private Rigidbody2D body;
     private ParticleSystem pSys;
+
+    private float xInput = 0.0f;
+    private float yInput = 0.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,23 +26,28 @@ public class test_anim_and_particles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x_input = Input.GetAxisRaw("Horizontal");
-        float y_input = Input.GetAxisRaw("Vertical");
+        mAnim.SetFloat("moveX", moveDirection.x);
+        mAnim.SetFloat("moveY", moveDirection.y);
+        mAnim.SetFloat("moveMag", Mathf.Abs(moveDirection.x));
+        mAnim.SetFloat("idleDir", idleDir);
 
-        Vector2 move_direction = new Vector2(Mathf.Abs(x_input) > 0.0f ? Mathf.Sign(x_input) : 0.0f, Mathf.Abs(y_input) > 0.0f ? Mathf.Sign(y_input) : 0.0f).normalized;
-
-        mAnim.SetFloat("moveX", move_direction.x);
-        mAnim.SetFloat("moveY", move_direction.y);
-        mAnim.SetFloat("moveMag", Mathf.Abs(move_direction.x));
-
-        if (Mathf.Abs(x_input) > 0.0f)
+        if (Mathf.Abs(xInput) > 0.0f)
         {
-            pSys.transform.localPosition = new Vector3(particleOffsetX * x_input, pSys.transform.localPosition.y, pSys.transform.localPosition.z);
-
-            movement = new Vector2(body.position.x + Mathf.Sign(x_input) * speed, body.position.y);
-            body.MovePosition(movement);
+            idleDir = Mathf.Sign(xInput);
+            pSys.transform.localPosition = new Vector3(particleOffsetX * xInput, pSys.transform.localPosition.y, pSys.transform.localPosition.z);
         }
 
+    }
+
+    private void FixedUpdate()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(Mathf.Abs(xInput) > 0.0f ? Mathf.Sign(xInput) : 0.0f, Mathf.Abs(yInput) > 0.0f ? Mathf.Sign(yInput) : 0.0f).normalized;
+
+        movement = new Vector2(moveDirection.x * speed, body.linearVelocity.y);
+        body.linearVelocity = movement;
     }
 
     public void OnFlatRoll()

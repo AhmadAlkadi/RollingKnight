bfb8b6d350b659c309e0b306ea97f6b48549482b
Nam Nguyen <namnh11@gmail.com>
2024-11-24 17:22:44 -0800

    Complete the HealthManager, update collision when player hit the enemy

--- Assets/Test Scripts/HealthManager.cs
+++ Assets/Test Scripts/HealthManager.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int health = 3;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    public int Health {
+        get {return health;}
+        set {health = Mathf.Clamp(value,0,3);}
+    }
+
+     private void OnEnable()
     {
-        
+        // Subscribe to sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from sceneLoaded event
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Reassign the hearts array when the scene is loaded
+        ReassignHeartReferences();
+    }
+
+    private void ReassignHeartReferences()
+    {
+        // Find the HeartsContainer and get all its child Image components
+        GameObject heartsContainer = GameObject.Find("Canvas/HeartsContainer");
+        if (heartsContainer != null)
+        {
+            hearts = heartsContainer.GetComponentsInChildren<Image>();
+        }
+        else
+        {
+            Debug.LogWarning("HeartsContainer not found!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (Image img in hearts)
+        {
+            img.sprite = emptyHeart;
+        }
+        for (int i = 0; i < health; i++)
+        {
+            hearts[i].sprite = fullHeart;
+        }
     }
 }
--- Assets/Test Scripts/Player.cs
+++ Assets/Test Scripts/Player.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using System.Collections;
 
 public class Player: MonoBehaviour
 {
     public PlayerSpriteRenderer normalRenderer;
+    private HealthManager healthManager;
 
     private DeathAnimation deathAnimation;
+    private bool isVulnerable = true;
+    
 
     public bool normal => normalRenderer.enabled;
 
     private void Awake(){
         deathAnimation = GetComponent<DeathAnimation>();
+        if(GameManager.Instance != null) {
+            healthManager = GameManager.Instance.GetComponent<HealthManager>();
+        }
+
+        // Handle missing HealthManager component
+        if (healthManager == null) {
+            Debug.LogError("HealthManager component not found on GameManager!");
+        }
     }
     public void Hit(){
-        Death();
+        if(isVulnerable) {
+            healthManager.health--;
+            if(healthManager.health <= 0) {
+                Death();
+            }else {
+                StartCoroutine(GetHurt());
+            }
+        }
+        
+    }
+
+    IEnumerator GetHurt(){
+        //Layer 3: Player
+        //Layer 8: Enemy
+        //Enable ignore collision
+        isVulnerable = false;
+        Physics2D.IgnoreLayerCollision(3,8,true);
+        yield return new WaitForSeconds(3);
+        //Disable ignore collision
+        Physics2D.IgnoreLayerCollision(3,8,false);
+        isVulnerable = true;
     }
 
     private void Death() {

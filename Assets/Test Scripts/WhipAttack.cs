cfd6201cac4e8a42c44312c5efacee59d7b6250b
Sean Butler <seanxbutler@gmail>
2024-11-28 00:09:59 -0800

    simply attack animation script
    add main character as prefab

--- Assets/Test Scripts/WhipAttack.cs
+++ Assets/Test Scripts/WhipAttack.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public Animator animator; // Reference to the Animator component
-    public string attackLeftAnimation = "left_ball_attack";
-    public string attackRightAnimation = "right_ball_attack";
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         // Check if the attack button is pressed
-        if (Input.GetButtonDown("Attack")) // ENTER key to attack with whip
+        if (Input.GetKeyDown(KeyCode.Return)) // ENTER key to attack with whip
         {
             // Trigger the attack animation
-            Debug.Log("Attack button pressed.");
+            //Debug.Log("Attack button pressed.");
             TriggerAttack();
         }
     }
@@ -21,20 +24,6 @@ public class PlayerAttack : MonoBehaviour
     // Function to trigger the attack animation
     private void TriggerAttack()
     {
-        
-        //float moveX = animator.GetFloat("moveX");
         animator.SetTrigger("Attack");
-
-        /*
-        if (moveX == 1) // Facing right
-        {
-            animator.SetTrigger(attackRightAnimation);
-        }
-        else if (moveX == -1) // Facing left
-        {
-            animator.SetTrigger(attackLeftAnimation);
-        }
-        */
-
     }
 }

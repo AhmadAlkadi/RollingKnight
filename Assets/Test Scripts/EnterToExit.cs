387f51773237b794f133f108c3d7d4618db30f9d
Ahmad Alkadi <alkadiahmad889@gmail.com>
2024-12-02 12:20:19 -0800

    End Scene has been added

--- Assets/Test Scripts/EnterToExit.cs
+++ Assets/Test Scripts/EnterToExit.cs
@@ -3,7 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public bool checker = true;
     public string loadSceneName = "";
+    public float time = 5.0f;
+    private bool runInvoke = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +16,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if (!runInvoke)
+        {
+            Invoke(nameof(allowExit), time);
+            runInvoke = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Return) && checker)
             SceneManager.LoadScene(loadSceneName);
+    }
+
+    public void allowExit()
+    {
+        checker = true;
     }
 }
